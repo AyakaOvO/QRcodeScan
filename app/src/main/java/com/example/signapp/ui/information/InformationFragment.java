c2d8531a73ae9fd27b43094efcfe027a0589cb26
AyakaOvO <Hnx18835378528@163.com>
2024-06-19 00:03:58 +0800

    0.6信息显示功能

--- app/src/main/java/com/example/signapp/ui/information/InformationFragment.java
+++ app/src/main/java/com/example/signapp/ui/information/InformationFragment.java
@@ -2,13 +2,16 @@ package com.example.signapp.ui.information;
 
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -17,12 +20,66 @@ import com.example.signapp.R;
 
 public class InformationFragment extends Fragment {
 
+    private SharedPreferences sharedPreferences;
+    private AppCompatActivity appCompatActivity;
+    private String name;
 
+    private String sclass;
+
+    private String id;
+
+    private String ralid;
+
+    private View view;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_information, container, false);
+         view = inflater.inflate(R.layout.fragment_information, container, false);
+
+
+        appCompatActivity = (AppCompatActivity) getActivity();
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(appCompatActivity);
+
+        name = sharedPreferences.getString("name","未登录");
+        sclass = sharedPreferences.getString("sclass","点击登录");
+        id = sharedPreferences.getString("id","0");
+        ralid =sharedPreferences.getString("ralid","1");
+
+        if(!name.equals("未登录") && !sclass.equals("点击登录")){
+            TextView nameText = view.findViewById(R.id.information_name_view);
+            nameText.setText(name);
+            TextView sclassText = view.findViewById(R.id.information_sclass_view);
+            sclassText.setText(sclass);
+            TextView idText = view.findViewById(R.id.information_id_view);
+            idText.setText(id);
+            TextView ralidText = view.findViewById(R.id.information_valid_view);
+            ralidText.setText(ralid);
+
+        }
+
+
         return view;
+    }
+
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        name = sharedPreferences.getString("name","未登录");
+        sclass = sharedPreferences.getString("sclass","点击登录");
+        id = sharedPreferences.getString("id","0");
+        ralid =sharedPreferences.getString("ralid","1");
+        if(!name.equals("未登录") && !sclass.equals("点击登录")){
+            TextView nameText = view.findViewById(R.id.information_name_view);
+            nameText.setText(name);
+            TextView sclassText = view.findViewById(R.id.information_sclass_view);
+            sclassText.setText(sclass);
+            TextView idText = view.findViewById(R.id.information_id_view);
+            idText.setText(id);
+            TextView ralidText = view.findViewById(R.id.information_valid_view);
+            ralidText.setText(ralid);
+
+        }
     }
 
     @Override
